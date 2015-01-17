41a94a171d815d6635435e16aa8ed11cf6bc2fdb
xiaoshan5733 <wengqianshan5733@gmail.com>
2015-01-17 23:03:05 +0800

    upload

--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 var fs = require('fs');
 var path = require('path');
 var crypto = require('crypto');
-
 //var request = require('request');//需要安装
 var mkdirp = require('mkdirp');//需要安装
 //var gaze = require('gaze');
+var qn = require('qn');
 
 var source = './a.jpg';//要监听的文件
 var md5Path = './md5.txt';//md5存放文件
+var client = qn.create({
+    accessKey: '_IklVVoJ6zfKvykBMpYZGix7wwJflOotajvcOv6X',
+    secretKey: 'wN9K9PqcWYmcX9e0P22ynZGzm_DU8rQfZLeJ_cNc',
+    bucket: 'wenglou',
+    domain: 'http://wenglou.qiniudn.com',
+    timeout: 3600000, // default rpc timeout: one hour, optional
+});
 
 //检查目录是否存在
 var dist = path.resolve(__dirname, 'data');//变化的文件存放目录
@@ -57,7 +64,17 @@ function read() {
         var second = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
         var dat = year + '' + month + '' + date + '' + hour + '' + minute + '' + second;
         var newFile = n + dat + '.' + s;
-        fs.writeFileSync(path.join('./data', newFile), fs.readFileSync(source));
+        //fs.writeFileSync(path.join('./data', newFile), fs.readFileSync(source));
+        client.uploadFile(path.join(__dirname, source), function(err, qf) {
+            var url = qf.url;
+            var time = qf['x:ctime'];
+
+            var log = url + ' : ' + time + '\n';
+            fs.appendFile(path.join(__dirname, './log.txt'), log, function(err) {
+                console.log(err)
+            });
+            //console.log(qf);
+        });
         //TODO 上传文件, 转移目录
         /*request.post({
             url: 'http://',
