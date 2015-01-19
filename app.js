533ca7edf9dea9bbd2c8628e96717f68d0ba7fe4
弱冠 <qianshan.wengqs@alibaba-inc.com>
2015-01-19 11:33:59 +0800

    文件压缩后md5，减小精确度

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var path = require('path');
 var crypto = require('crypto');
 var mkdirp = require('mkdirp');
 var qn = require('qn');
+var lwip = require('lwip');
+var moment = require('moment');
 var config = require('./config');
+
+
 
 var client = qn.create(config.upload);
 
@@ -33,47 +37,60 @@ function md5File(filename) {
   return sum.digest('hex');
 };
 
-function read() {
-    var md5 = md5File(config.source);
-    if (md5 !== lastMd5) {
-        console.log('文件有变化，新文件md5：' + md5)
-        lastMd5 = md5;
-        fs.writeFileSync(config.md5Path, md5);
-        var index = config.source.lastIndexOf('.');
-        var n = config.source.substr(0, index);
-        var s = config.source.substr(index + 1, config.source.length);
-        //TODO 用moment
-        var d = new Date();
-        var year = d.getFullYear();
-        var month = (d.getMonth() + 1) < 10 ? '0' + (d.getMonth() + 1) : d.getMonth() + 1;
-        var date = d.getDate() < 10 ? '0' + d.getDate() : d.getDate();
-        var hour = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
-        var minute = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-        var second = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
-        var dat = year + '' + month + '' + date + '' + hour + '' + minute + '' + second;
-        var newFile = n + dat + '.' + s;
-        //fs.writeFileSync(path.join('./data', newFile), fs.readFileSync(config.source));
-        client.uploadFile(path.join(__dirname, config.source), function(err, qf) {
-            if (err) {
-                console.log(err, qf, '上传出错了');
-                var log = 'Error: ' + err + JSON.stringify(qf) + '@' + Date.now();
-                fs.appendFile(config.logPath, log, function(err) {
-                    //console.log(err)
-                });
-                return;
-            }
-            var url = qf.url;
-            var time = qf['x:ctime'];
+function process(md5) {
+    console.log('文件有变化，新文件md5：' + md5)
+    lastMd5 = md5;
+    fs.writeFileSync(config.md5Path, md5);
 
-            var log = url + ' : ' + time + '\n';
-            fs.appendFile(config.logPath, log, function(err) {
-                console.log(err)
-            });
-            //console.log(qf);
-        });
-    } else {
-        console.log('文件没有变化');
+    //保存到本地
+    if (config.saveLoal) {
+        var ext = path.extname(config.source);
+        var base = path.basename(config.source, ext);
+        var d = moment().format('YYYYMMDDHHmmss');
+        var newFile = base + d + ext;
+        fs.writeFileSync(path.join(config.dist, newFile), fs.readFileSync(config.source));    
     }
+
+    client.uploadFile(config.source, function(err, qf) {
+        if (err) {
+            console.log(err, qf, '上传出错了');
+            var log = 'Error: ' + err + JSON.stringify(qf) + '@' + Date.now();
+            fs.appendFile(config.logPath, log, function(err) {
+                //console.log(err)
+            });
+            return;
+        }
+        var url = qf.url;
+        var time = qf['x:ctime'];
+
+        var log = url + ' : ' + time + '\n';
+        fs.appendFile(config.logPath, log, function(err) {
+            console.log(err)
+        });
+        //console.log(qf);
+    });
+}
+
+function read() {
+    //缩小图片对比像素
+    lwip.open(config.source, function(err, image) {
+        image.resize(20, 20, function(err, image) {
+            image.toBuffer('jpg', function(err, buffer) {
+                //文件md5
+                var sum = crypto.createHash('md5');
+                sum.update(buffer);
+                var md5 = sum.digest('hex');
+                if (md5 !== lastMd5) {
+                    process(md5);
+                    /*image.writeFile('./output.jpg', function(err) {
+                        console.log(err)
+                    })*/
+                } else {
+                    console.log('文件没有变化:lwip');
+                }
+            })
+        })
+    });
 }
 
 setInterval(function() {
--- config.default.js
+++ config.default.js
@@ -1,7 +1,8 @@
 var path = require('path');
 module.exports = {
-    source: 'a.jpg',
-    dist: path.join(__dirname, 'data'),
+    source: './a.jpg',
+    saveLoal: true,//是否保存到本地
+    dist: path.join(__dirname, 'data'),// saveLoal为true才生效
     md5Path: path.join(__dirname, './md5.txt'),
     logPath: path.join(__dirname, './log.txt'),
     upload: {
